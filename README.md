b300bab63de5c56f111377eb2bf93857b279eda8
Jose Hiraldo <hiraldo06@gmail.com>
2018-05-29 00:17:40 -0400

    Subiendo el proyecto por primera vez

---
+++ README.md
@@ -0,0 +1,4 @@
+TaskApp
+=======
+
+A Symfony project created on May 28, 2018, 10:11 pm.
---
+++ src/AppBundle/Controller/Usuario/UsuarioController.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HiraldoTran
+ * Date: 11/6/18
+ * Time: 6:33 PM
+ */
+
+namespace AppBundle\Controller\Usuario;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class UsuarioController extends Controller
+{
+    /**
+     * @Route("/usuario", name="vista_usuario")
+     */
+    public function indexUsuario(){
+
+        /*$em=$this->getDoctrine()->getManager();
+
+        $usuario=new Usuario();
+        $usuario->setNombre("Ivan Mendoza");
+        $usuario->setUsername("ivanm@gmail.com");
+
+        $em->persist($usuario);
+        $em->flush();
+        dump( $usuario);
+        die;*/
+
+        $usuarios=$this->getDoctrine()->
+        getRepository(Usuario::class)
+        ->findAll();
+
+        return $this->render('@App/Usuario/lista.html.twig', [
+            'usuarios' => $usuarios
+        ]);
+        //dump($usuarios);
+        //die;
+
+    }
+
+
+    /**
+     * @Route("/usuario/{idUsuario}", name="informacion_usuario")
+     */
+    public function indexUsuarioInfo($idUsuario){
+        /*dump( "aqui estamos con el id del usario : ".$idUsuario);
+        die;*/
+        return $this->render('@App/Usuario/index.html.twig', [
+            'idUsuario' => $idUsuario
+        ]);
+    }
+}
