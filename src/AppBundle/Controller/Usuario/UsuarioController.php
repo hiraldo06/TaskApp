d9761ea5b092ab2b2dc9979f11ef66cda30fbb98
Jose Hiraldo <hiraldo06@gmail.com>
2018-05-29 22:27:26 -0400

    agregando el template y librerias del mismo

--- src/AppBundle/Controller/Usuario/UsuarioController.php
+++ src/AppBundle/Controller/Usuario/UsuarioController.php
@@ -9,6 +9,7 @@
 namespace AppBundle\Controller\Usuario;
 
 
+use AppBundle\Entity\Usuario;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
